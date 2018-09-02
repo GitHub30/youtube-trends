5386184327f1b0b9ae4f206334cfe9bf4a16dd57
Tomofumi Inoue <funaox@gmail.com>
2018-09-02 13:10:11 +0900

    first

---
+++ nuxt.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'youtube-trends',
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'Nuxt.js project'}
+    ],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: {color: '#3B8070'},
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend(config, {isDev, isClient}) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  modules: [
+    // この行を追加して有効化
+    '@nuxtjs/vuetify'
+  ],
+
+  vuetify: {
+    // Vuetify の設定はここに書く
+    theme: {
+      primary: '#3f51b5',
+      secondary: '#b0bec5',
+      accent: '#8c9eff',
+      error: '#b71c1c'
+    }
+  }
+}
